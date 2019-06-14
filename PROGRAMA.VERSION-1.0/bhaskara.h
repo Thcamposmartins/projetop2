d7cac285fb0d83db93c20f3f2d97347e491bb139
Thcamposmartins <51833404+Thcamposmartins@users.noreply.github.com>
2019-06-14 15:24:58 -0300

    Update bhaskara.h
    atualização do retorno menu

--- PROGRAMA.VERSION-1.0/bhaskara.h
+++ PROGRAMA.VERSION-1.0/bhaskara.h
@@ -1,8 +1,18 @@
+#include<iostream>
+#include<cmath>
+#include <stdlib.h>
+#include<math.h>
+#include<conio.h>
+#include<string.h>
+#include<stdio.h>
+#include<cstdlib>
 #ifndef BHASKARA_H_INCLUDED
 #define BHASKARA_H_INCLUDED
 void bhaskara(int& op);
+
 void bhaskara(int& op) {
 	float a, b, c;
+	char voltar;
 	printf("Digite os valores de a,b,c: ");
 	scanf("%f %f %f", &a, &b, &c);
 
@@ -11,7 +21,21 @@ void bhaskara(int& op) {
 	printf("\nA raiz de x1: %.2f\n", x);
 	printf("\nA raiz de x2: %.2f\n", y);
 	system("pause");
-}
-
-
+	printf("\nDeseja voltar para o menu? (s/n)");
+	scanf("%s",&voltar);
+	if (voltar == 's') {
+		system('cls');
+		menu(op);
+	}
+	else	{
+		system('cls');
+		printf("Deseja refazer a operaçăo? (s/n)");
+		scanf("%s",&voltar);
+		if (voltar == 's') {
+			bhaskara(op);
+		}
+		else		{
+		system('exit');
+		}	}}
 #endif // BHASKARA_H_INCLUDED
+
